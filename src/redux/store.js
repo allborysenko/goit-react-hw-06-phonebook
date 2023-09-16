d3326b7e035258eb760c0b902e53371c9f204dc7
Aleksandr Borysenko <borysenko343@gmail.com>
2023-09-16 17:31:33 +0300

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactSlice } from './contactSlise';
 import { filterSlice } from './filterSlice';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedContactsReducer = persistReducer(
+  persistConfig,
+  contactSlice.reducer
+);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
